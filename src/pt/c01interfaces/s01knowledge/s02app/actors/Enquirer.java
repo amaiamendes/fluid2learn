7b943fd97b2480f2dbf9fab748722749e03380a9
Arthur Mendes <amaiamendes@gmail.com>
2015-03-21 23:06:04 -0300

    atualizacao1

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -6,38 +6,138 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IDeclaracao;
 import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
+import java.util.Vector;
+import java.util.Collections;
 
 public class Enquirer implements IEnquirer
 {
-    IObjetoConhecimento obj;
+    IObjetoConhecimento obj1, obj2;
 	
 	public Enquirer()
 	{
+
 	}
-	
+
 	
 	@Override
 	public void connect(IResponder responder)
 	{
+        int i, j, k, flag1, flag2, flag3;
+        String pergunta, respostaEsperada, resposta, teste1, teste2;
         IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
+        IDeclaracao decl1, decl2;
+        String animalsOriginal[] = bc.listaNomes();
+        Vector <String> animals = new Vector<String>();
+        Collections.addAll(animals, animalsOriginal);
 
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
+        Vector <String> perguntasFeitas = new Vector <String>();
+
+
+        for (i = 0; i < animals.size(); i++) // loop sobre a lista de animais
+        {
+
+            obj1 = bc.recuperaObjeto(animals.get(i)); // pega um animal na lista de animais
+            decl1 = obj1.primeira(); // pega a primeira pergunta e resposta sobre esse animal
+
+            flag1 = 1; // flag que indica se a resposta referente ao animal do Responder é igual a do animal que estamos chutando para uma dada pergunta
+            flag2 = 0; // flag que indica se a resposta referente ao animal do Responder é oposta a do animal que estamos chutando para uma dada pergunta
+
+            while (decl1 != null && flag1 != 0 && animals.size() > 1) // loop sobre a lista de perguntas e respostas dos animais
+            {
+                pergunta = decl1.getPropriedade(); // pega uma pergunta sobre o animal
+                respostaEsperada = decl1.getValor(); // pega a resposta correspondente para esse animal
+
+                flag3 = 0; // flag que indica se a pergunta já foi feita antes
+
+                for (k = 0; k < perguntasFeitas.size(); k++) // loop sobre o vetor de perguntas ja feitas
+                    if (pergunta.equalsIgnoreCase(perguntasFeitas.get(k)))
+                        flag3 = 1;
+
+                if (flag3 == 1) // se a pergunta já foi feita, pula-se para a proxima
+                {
+                    decl1 = obj1.proxima();
+                    continue;
+                }
+
+                perguntasFeitas.addElement(pergunta); // adiciona a pergunta ao vetor de perguntas já feitas
+
+                resposta = responder.ask(pergunta); // procura a resposta referente ao animal do Responder para uma dada pergunta
+                if (resposta.equalsIgnoreCase(respostaEsperada))
+                    flag1 = 1;
+                else
+                {
+                    if (resposta.equalsIgnoreCase("nao") || resposta.equalsIgnoreCase("sim"))
+                        flag2 = 1;
+
+                    flag1 = 0;
+                }
+
+                for (j = i+1; j < animals.size();) // loop sobre o resto da lista de animais para eliminar elementos
+                {
+
+                    obj2 = bc.recuperaObjeto(animals.get(j)); // pega um animal do resto da lista de animais
+                    decl2 = obj2.primeira(); // pega a primeira pergunta e resposta referente a esse animal
+
+                    flag3 = 0; // flag que indica se a pergunta e a resposta são iguais a do animal no enquirer ou se a resposta é oposta
+
+                    while (decl2 != null) // loop sobre as perguntas e respostas do animal
+                    {
+                        teste1 = decl2.getPropriedade();
+                        teste2 = decl2.getValor();
+
+                        if (teste1.equalsIgnoreCase(pergunta))
+                        {
+                            if (teste2.equalsIgnoreCase(respostaEsperada))
+                                flag3 = 1;
+                            else
+                                flag3 = 2;
+                            break;
+                        }
+                        else
+                            decl2 = obj2.proxima();
+                    }
+
+                    /*pergunta e resposta do animal no enquirer conferem com o no responder e
+                    * ou a pergunta não é encontrada no animal aqui
+                    * ou é encontrada, mas a resposta é oposta
+                    * */
+                    if ((flag1 == 1 && (decl2 == null || flag3 == 2)))
+                        animals.removeElementAt(j);
+
+                    /*resposta do animal no enquirer não confere com o no responder e
+                    * resposta do responder à pergunta é não sei e
+                    * a pergunta é encontrada no animal aqui
+                    * */
+                    else if (flag1 == 0 && (flag2 == 0 && decl2 != null))
+                        animals.removeElementAt(j);
+
+                    /*resposta do animal no enquirer não confere com o no responder e
+                    * resposta do responder é oposta à esperada pelo enquirer e
+                    * ou a pergunta não foi encontrada no animal aqui,
+                    * ou a pergunta foi encontrada, mas também é oposta à do animal no responder
+                    * */
+                    else if (flag1 == 0 && (flag2 == 1 && (decl2 == null || flag3 == 1)))
+                        animals.removeElementAt(j);
+
+                    else
+                        j++;
+                }
+                decl1 = obj1.proxima();
+
+                if (flag1 == 0)
+                {
+                    animals.removeElementAt(i);
+                    i--;
+                }
+            }
+            if (animals.size() == 1)
+            {
+                i = 0;
+                break;
+            }
+        }
+
+		boolean acertei = responder.finalAnswer(animals.get(i));
 		
 		if (acertei)
 			System.out.println("Oba! Acertei!");
--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -1,46 +1,132 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
 
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 
 public class EnquirerMaze implements IEnquirer {
 
-	IResponder responder;
-	
-	public void connect(IResponder responder) {
-		this.responder = responder;
-	}
-	
-	public boolean discover() {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-		String tipo = scanner.nextLine();
-		while (!tipo.equalsIgnoreCase("F")) {
-		   System.out.print("  --> ");
-		   String pc = scanner.nextLine();
-		   switch (tipo.toUpperCase()) {
-		      case "P": String resposta = responder.ask(pc);
-		                System.out.println("  Resposta: " + resposta);
-		                break;
-		      case "M": boolean moveu = responder.move(pc);
-		                System.out.println((moveu)?"  Movimento executado!":"Não é possível mover");
-		                break;
-		   }
-			System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-			tipo = scanner.nextLine();
-		}
-		
-		if (responder.finalAnswer("cheguei"))
-			System.out.println("Você encontrou a saida!");
-		else
-			System.out.println("Fuém fuém fuém!");
-		
-		scanner.close();
-		
-		return true;
-	}
-	
+    IResponder responder;
+
+    int x = 0, y = 0;
+
+    Map<String, Integer> maze = new HashMap <String, Integer>();
+
+    public void connect(IResponder responder) {
+        this.responder = responder;
+    }
+
+    public boolean discover() {
+
+        solve();
+
+        if (responder.finalAnswer("cheguei")) {
+
+            System.out.println("Você encontrou a saida!");
+
+        } else {
+
+            System.out.println("Fuém fuém fuém!");
+        }
+
+        return true;
+    }
+
+    private boolean solve() {
+
+        int i;
+
+        if (responder.ask("aqui").equalsIgnoreCase("saida")) {
+
+            return true;
+        }
+
+        for (i = 0; i < 4; i++) {
+
+            if (!maze.containsKey(x + ";" + y)) {
+
+                maze.put(x + ";" + y, 1);
+            }
+
+            if (i == 0) {
+
+                if ((responder.ask("norte").equalsIgnoreCase("passagem") || responder.ask("norte").equalsIgnoreCase("saida"))
+                        && !maze.containsKey(x + ";" + (y + 1))) {
+
+                    responder.move("norte");
+
+                    y++;
+
+                    if (solve()) {
+
+                        return true;
+                    }
+                    else {
+
+                        responder.move("sul");
+                    }
+                }
+            }
+            else if (i == 1) {
+
+                if ((responder.ask("leste").equalsIgnoreCase("passagem") || responder.ask("leste").equalsIgnoreCase("saida")) && !maze.containsKey((x + 1) + ";" + y)) {
+
+                    responder.move("leste");
+
+                    x++;
+
+                    if (solve()) {
+
+                        return true;
+                    }
+                    else {
+
+                        responder.move("oeste");
+                    }
+                }
+            }
+            else if (i == 2) {
+
+                if ((responder.ask("sul").equalsIgnoreCase("passagem") || responder.ask("sul").equalsIgnoreCase("saida")) && !maze.containsKey(x + ";" + (y - 1))) {
+
+                    responder.move("sul");
+
+                    y--;
+
+                    if (solve()) {
+
+                        return true;
+                    }
+                    else {
+
+                        responder.move("norte");
+                    }
+                }
+            }
+            else {
+
+                if ((responder.ask("oeste").equalsIgnoreCase("passagem") || responder.ask("oeste").equalsIgnoreCase("saida")) && !maze.containsKey((x - 1) + ";" + y)) {
+
+                    responder.move("oeste");
+
+                    x--;
+
+                    if (solve()) {
+
+                        return true;
+                    }
+                    else {
+
+                        responder.move("leste");
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
